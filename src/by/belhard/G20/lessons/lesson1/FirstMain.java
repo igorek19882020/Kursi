dba1ae40b36b61bd4b6ff92678e95d08e7f268f1
RitaVeikuts <ritaveikuts@gmail.com>
2020-01-27 21:04:11 +0300

    init

---
+++ src/by/belhard/G20/lessons/lesson1/FirstMain.java
@@ -0,0 +1,11 @@
+package by.belhard.G20.lessons.lesson1;
+
+public class FirstMain {
+
+    public static void main(String[] args) {
+
+            System.out.println("Hello G20!!!");
+        }
+
+    }
+
