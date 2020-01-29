46477505fe48ec73fac585099b69ac5ad5948bed
igorek19882020 <https://github.com/igorek19882020/Kursi.git>
2020-01-29 22:00:01 +0300

    lesson2

---
+++ src/by/belhard/G20/lessons/lesson2/Task.java
@@ -0,0 +1,25 @@
+package by.belhard.G20.lessons.lesson2;
+
+public class Task {
+    public static void main(String[] args) {
+        //int  a = 1;
+        /*if (a>5) {
+            System.out.println("a>b");
+            a++;
+
+        */
+        int b = 0;
+        for(int i = 10;i>0;i -= 2) {
+            System.out.print(i + " ");
+        b = i;
+}
+
+
+
+
+        System.out.println("\n\n" + b);
+    }
+
+
+
+}
