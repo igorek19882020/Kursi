d6e40221eb420d8cfe90ed81f451774cbf2f8d9b
Igor <igorek20081988@mail.ru>
2020-01-29 19:25:52 +0300

    34

--- src/by/belhard/G20/lessons/lesson2/SecondLesson.java
+++ src/by/belhard/G20/lessons/lesson2/SecondLesson.java
@@ -1,4 +1,18 @@
 package by.belhard.G20.lessons.lesson2;
 
 public class SecondLesson {
+
+    public static void main(String[] args) {
+
+
+        byte b1;
+        b1 = 127;
+        byte b2 = 2;
+       // byte b3 = b1 + b2;
+        byte b3 = (byte) (b1 + b2);
+
+        System.out.println(b3);
+    }
+
+
 }
