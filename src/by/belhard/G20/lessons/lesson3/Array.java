e1cf2b6c73bf18934d8e6f5bf113038affc1c3ec
igorek19882020 <https://github.com/igorek19882020/Kursi.git>
2020-02-03 20:17:33 +0300

    lesson3

--- src/by/belhard/G20/lessons/lesson3/Array.java
+++ src/by/belhard/G20/lessons/lesson3/Array.java
@@ -4,14 +4,30 @@ public class Array {
     public static void main(String[] args) {
         //int [] ar1 = {2,3,67,56,55};
         int []  ar2 = new int[30];
+        int []  ar3 = new int [4];
+
+
         for (int i = 0;i<ar2.length;i++) {
-            ar2[i] =i;
+            ar2[i] =i*2;
+       // System.out.print("  "+ ar2[i]);
+    }
+
+        String [] stringArray = {"one", "two","three","four","five"};
 
 
 
 
 
-        System.out.print("  "+ ar2[i]);
+    for (String  s: stringArray) {
+if ("three".equals(s))
+    continue;
+        System.out.println(s);
+    }
 
 
-    }}}
+
+    }
+
+
+
+}
