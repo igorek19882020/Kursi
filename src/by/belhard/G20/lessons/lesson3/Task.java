2aa1d18247ee77666285d54fd60e5f8793359f3c
igorek19882020 <https://github.com/igorek19882020/Kursi.git>
2020-02-03 19:36:24 +0300

    lesson3

--- src/by/belhard/G20/lessons/lesson3/Task.java
+++ src/by/belhard/G20/lessons/lesson3/Task.java
@@ -2,9 +2,41 @@ package by.belhard.G20.lessons.lesson3;
 
 public class Task {
     public static void main(String[] args) {
-        double u = Math.PI ;
-        System.out.println(u*u);
+
+
+        int i = 7;
+
+        switch (i) {
+
+            case 1:
+                System.out.println("Monday  ");
+                break;
+
+
+            case 2:
+                System.out.println("thuesday    ");
+                break;
+
+            case 3:
+                System.out.println("Wensd");
+                break;
+            case 4 :
+                System.out.println("Thursday");
+                break;
+            case 5 :
+                System.out.println("Friday");
+                break;
+            case 6 :
+               // System.out.println("Saturday");
+                break;
+            case 7 :
+                System.out.println("Sunday");
+                break;
+        }
+
+
+        //System.out.println();
+
+
     }
-
-
 }
