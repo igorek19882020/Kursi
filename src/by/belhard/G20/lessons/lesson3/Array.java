dd5bb975353a37a3fd593ae0be386f50fe52f51a
igorek19882020 <https://github.com/igorek19882020/Kursi.git>
2020-02-03 19:50:48 +0300

    lesson3

---
+++ src/by/belhard/G20/lessons/lesson3/Array.java
@@ -0,0 +1,17 @@
+package by.belhard.G20.lessons.lesson3;
+
+public class Array {
+    public static void main(String[] args) {
+        //int [] ar1 = {2,3,67,56,55};
+        int []  ar2 = new int[30];
+        for (int i = 0;i<ar2.length;i++) {
+            ar2[i] =i;
+
+
+
+
+
+        System.out.print("  "+ ar2[i]);
+
+
+    }}}
