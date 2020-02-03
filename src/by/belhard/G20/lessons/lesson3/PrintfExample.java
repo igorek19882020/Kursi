fcf913d67edcf5ab46f045e981370ac711836822
igorek19882020 <https://github.com/igorek19882020/Kursi.git>
2020-02-03 21:24:53 +0300

    lesson3

--- src/by/belhard/G20/lessons/lesson3/PrintfExample.java
+++ src/by/belhard/G20/lessons/lesson3/PrintfExample.java
@@ -4,8 +4,11 @@ public class PrintfExample {
     public static void main(String[] args) {
 
         String name = "Igor";
-        System.out.printf("I am %d   years old.My name is %s  ",30,name);
+        System.out.printf("I am %d   years old.My name is %10.10s  ",30,name);
 
 
+        System.out.println();
+
+        int n = 5;
     }
 }
