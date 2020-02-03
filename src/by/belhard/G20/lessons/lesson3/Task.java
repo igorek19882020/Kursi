c9200ecd8126f3e40e6a6a5c16d8d5900f2a820c
igorek19882020 <https://github.com/igorek19882020/Kursi.git>
2020-02-03 19:07:08 +0300

    lesson3

---
+++ src/by/belhard/G20/lessons/lesson3/Task.java
@@ -0,0 +1,10 @@
+package by.belhard.G20.lessons.lesson3;
+
+public class Task {
+    public static void main(String[] args) {
+        double u = Math.PI ;
+        System.out.println(u*u);
+    }
+
+
+}
