7d775d9afde4279ba4f423df65fd95a26c05bf47
igorek19882020 <https://github.com/igorek19882020/Kursi.git>
2020-02-03 21:01:49 +0300

    lesson3

--- src/by/belhard/G20/lessons/lesson3/Array.java
+++ src/by/belhard/G20/lessons/lesson3/Array.java
@@ -2,32 +2,57 @@ package by.belhard.G20.lessons.lesson3;
 
 public class Array {
     public static void main(String[] args) {
-        //int [] ar1 = {2,3,67,56,55};
-        int []  ar2 = new int[30];
-        int []  ar3 = new int [4];
 
+       String [] strAr = {"djsf" ,"f233 ",  "dfq32561","t231","dopo9","k643" };
+       String result = "";
 
-        for (int i = 0;i<ar2.length;i++) {
-            ar2[i] =i*2;
-       // System.out.print("  "+ ar2[i]);
-    }
-
-        String [] stringArray = {"one", "two","three","four","five"};
+       for (String s :strAr) {
+           if (s.length()<=3)
+               result+=s;       }
+        System.out.println(result);
 
 
 
 
+        //--------------------------------------------
+        /*int[][] mt = new int[8][8];
+        for (int i = 2; i <= 9; i++) {
+            for (int j = 2; j <= 9; j++) {
+                mt[i - 2][j - 2] = i * j;
+            }
+        }
+        for (int [] inArr : mt) {
+            for (int i : inArr) {
+                System.out.println(i + " ");
+            }
+                System.out.println();
+            }
+        System.out.println();
 
-    for (String  s: stringArray) {
+
+-------------------------------------------------
+
+        /*   int[] ar2 = new int[30];
+        int[] ar3 = new int[4];
+        int [][] ar = new int[3][3];
+        ar [0] = new int[3];
+        ar[1] = new int[]{4,5};
+        ar[2] = new int[1];
+
+
+     /*   for (int i = 0; i < ar.length; i++) {
+            ar[i] = i;
+            System.out.print("  " + ar[i]);
+        }
+
+        //   String [] stringArray = {"one", "two","three","four","five"};
+
+
+   /* for (String  s: stringArray) {
 if ("three".equals(s))
     continue;
         System.out.println(s);
+    }*/
+
+        }
     }
-
-
-
-    }
-
-
-
-}
