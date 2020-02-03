1723c4b7167e1d4ae599de0aaa5d55d6dbcfe645
igorek19882020 <https://github.com/igorek19882020/Kursi.git>
2020-02-03 21:07:52 +0300

    lesson3

---
+++ src/by/belhard/G20/lessons/lesson3/PrintfExample.java
@@ -0,0 +1,11 @@
+package by.belhard.G20.lessons.lesson3;
+
+public class PrintfExample {
+    public static void main(String[] args) {
+
+        String name = "Igor";
+        System.out.printf("I am %d   years old.My name is %s  ",30,name);
+
+
+    }
+}
