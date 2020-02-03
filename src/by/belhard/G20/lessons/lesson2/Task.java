a65b9046b4badcfa104b31e0f05a5e7abda3743a
igorek19882020 <https://github.com/igorek19882020/Kursi.git>
2020-02-03 18:38:44 +0300

    lesson2

--- src/by/belhard/G20/lessons/lesson2/Task.java
+++ src/by/belhard/G20/lessons/lesson2/Task.java
@@ -2,24 +2,16 @@ package by.belhard.G20.lessons.lesson2;
 
 public class Task {
     public static void main(String[] args) {
-        //int  a = 1;
-        /*if (a>5) {
-            System.out.println("a>b");
-            a++;
-
-        */
-        int b = 0;
-        for(int i = 10;i>0;i -= 2) {
-            System.out.print(i + " ");
-        b = i;
-}
+        double r = 20;
+        double s = 3.14 * r * r;
+        double l = 2 * 3.14 * r;
 
 
+        System.out.println("Площадь круга = " +s);
+        System.out.println("Длина окружности = " + l);
 
-
-        System.out.println("\n\n" + b);
     }
 
-
-
 }
+
+
